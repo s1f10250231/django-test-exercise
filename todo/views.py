a874c32fbd4fa89d29ff3c910a5f076c88a42f7c
1f10250231 <1f102502317@iniad.org>
2026-06-18 15:27:46 +0900

    トップページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils.timezone import make_aware
+from django.utils.dateparse import parse_datetime
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'])
+        task.save()
+
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks' : tasks
+    }
+    return render(request, 'todo/index.html', context)
